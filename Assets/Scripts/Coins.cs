d98d3a01f28b9b69b629233c61d5d6ad91eca00f
WhyAmIHere22 <cohle.contardo@gmail.com>
2024-09-03 00:08:03 -0400

    More assets
    More assets such as tilesets and more importantly backrounds

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour, IItem
+{
+    public static event Action<int> OnCoinCollect;
+    public int worth = 5;
+    public void Collect()
+    {
+        OnCoinCollect.Invoke(worth);
+        Destroy(gameObject);
+    }
+} 
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    float horizontalMovement;
+
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+
+    [Header("Groundcheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (isGrounded())
+        {
+            if (context.performed)
+
+            {
+                //Hold Down Jump Button = Full Height
+                rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            }
+            else if (context.performed)
+            {
+                //Light tap of Jump Button = Half the Height
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            }
+        }
+    }
+
+   private bool isGrounded()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color =Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+    }
+}
