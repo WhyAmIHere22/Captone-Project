e6b899b7ec2182af324251d0a5c4f5e0df49239e
WhyAmIHere22 <cohle.contardo@gmail.com>
2024-09-17 18:14:43 -0400

    Level switcing
    implemented level switching, code needs to be modiifed for extra levels

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,12 +7,19 @@ public class GameController : MonoBehaviour
 {
     int progressAmount;
     public Slider progressSlider;
+
+    public GameObject player;
+    public GameObject LoadCanvas;
+    public List<GameObject> levels;
+    private int currentLevelIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
         progressAmount = 0;
         progressSlider.value = 0;
         Coins.OnCoinCollect += IncreaseProgressAmount;
+        HoldToLoadLevel.OnHoldComplete += LoadNextLevel;
+        LoadCanvas.SetActive(false);
     }
 
 
@@ -23,13 +30,23 @@ public class GameController : MonoBehaviour
         if(progressAmount >= 100)
         {
             //Level Complete!
+            LoadCanvas.SetActive(true);
             Debug.Log("Level Complete`");
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoadNextLevel()
     {
-        
+        int nextLevelIndex = (currentLevelIndex == levels.Count - 1) ? 0 : currentLevelIndex + 1;
+        LoadCanvas.SetActive(false);
+
+        levels[currentLevelIndex].gameObject.SetActive(false);
+        levels[nextLevelIndex].gameObject.SetActive(true);
+
+        player.transform.position = new Vector3(0, 0, 0);
+
+        currentLevelIndex = nextLevelIndex;
+        progressAmount = 0;
+        progressSlider.value = 0;
     }
 }
